af346e0d13b6ca00e3920418007ef95e1f4cba99
Matt Bittinger <mattsbitt@gmail.com>
2022-12-20 16:07:53 -0600

    VSCODE setup to run react app with linting and SASS css working

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div className="my-element"><h1>Testing</h1></div>
+      <div>
+            <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+             </div>
+              <h1>Vite + React</h1>
+        <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> aasdfasdfasdfsand asdfasdf save to tesasdfasdft HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more.  Hope this really works.  Time to test again!!!
+      </p>asdf
+    </div>
+  )
+}
+
+export default App
+
+
+
